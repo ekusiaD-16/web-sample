65a0d6e334c05eb2e605964d7096927186d98bb6
Daisuke Sakaguchi <91122360+ekusiaD-16@users.noreply.github.com>
2024-06-19 04:47:40 +0000

    day/week/month routing

--- diary/src/app/app.routes.ts
+++ diary/src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { DayComponent } from './day/day.component';
+import { WeekComponent } from './week/week.component';
+import { MonthComponent } from './month/month.component';
 
 export const routes: Routes = [
-    { path: "", component : AppComponent}
+    { path: "",     component: WeekComponent},
+    { path: "day",  component: DayComponent},
+    { path: "week", component: WeekComponent},
+    { path: "month",component: MonthComponent},
 ];
+
--- diary/src/app/body/body.component.ts
+++ diary/src/app/body/body.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { Router, RouterOutlet } from '@angular/router';
+import { routes } from '../app.routes';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    MatButtonToggleModule,
+  ],
   templateUrl: './body.component.html',
   styleUrl: './body.component.scss'
 })
 export class BodyComponent {
 
+  constructor(private router: Router) {}
+  onChange(value:string) {
+    this.router.navigateByUrl(value)
+  }
 }
