92a9f1bfaa98c9926a3ebbdff7fc80d00c21f697
Daisuke Sakaguchi <91122360+ekusiaD-16@users.noreply.github.com>
2024-06-19 04:00:09 +0000

    footer template

--- diary/src/app/app.component.ts
+++ diary/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,7 @@ import { BodyComponent } from './body/body.component';
     RouterOutlet,
     HeaderComponent,
     BodyComponent,
+    FooterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
