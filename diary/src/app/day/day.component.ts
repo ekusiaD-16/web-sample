21b421605dfc7e6ee5bac277bd0e88d0d4d6a13c
Daisuke Sakaguchi <91122360+ekusiaD-16@users.noreply.github.com>
2024-06-20 06:25:21 +0000

    day form

--- diary/src/app/day/day.component.ts
+++ diary/src/app/day/day.component.ts
@@ -2,12 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { DiaryService } from '../servise/diary.service';
 import { Diary } from '../model/diary';
 import { CustomModule } from '../custom/custom.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-day',
   standalone: true,
   imports: [
     CustomModule,
+    MatCardModule,
+    MatSlideToggle,
+    FormsModule,
   ],
   providers: [
     DiaryService,
@@ -16,15 +22,26 @@ import { CustomModule } from '../custom/custom.module';
   styleUrl: './day.component.scss'
 })
 export class DayComponent implements OnInit {
-  diaries : Diary[] = [new Diary("xx","mm")];
+  diaries : Diary[] = [];
+  diary   : Diary;
   message : string = "";
 
-  constructor(private diaryService: DiaryService) {}
+  isWrite : boolean = false;
+
+  constructor(private diaryService: DiaryService) {
+    this.diary = new Diary("","");
+  }
 
   ngOnInit(): void {
+    const today = "xxxx-xx-xx";
     this.diaryService.getDiaries()
     .subscribe( diaries => {
       this.diaries = diaries;
+      this.diaries.forEach(diary => {
+        if(diary.date==today) {
+          this.diary = diary;
+        }
+      });
     });
   }
 
--- diary/src/app/model/diary.ts
+++ diary/src/app/model/diary.ts
@@ -1,11 +1,15 @@
 
 export class Diary {
     date : string;
+    toilet : number[];
+    bodyTemp : number;
     memo : string;
 
     constructor(date:string,memo:string) {
         this.date = date;
         this.memo = memo;
+        this.toilet = [0,0];
+        this.bodyTemp = 0;
     }
 
 }
